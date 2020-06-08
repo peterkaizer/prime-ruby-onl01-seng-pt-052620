2e2b0fd37c167874d753e906db797cc6bfb552c8
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-08 03:46:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+   flag = true
+   for x in 2..num-1
+       if num % x == 0
+           flag = false
+       end
+   end
+   return false if num < 2
+   return flag
+end
