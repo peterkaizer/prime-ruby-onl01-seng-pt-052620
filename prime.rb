dbd22ba8450b454277ab307dc7f41ffcc318760d
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-08 03:45:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(num)
-   return false if num < 2
    flag = true
    for x in 2..num-1
        if num % x == 0
            flag = false
        end
    end
+   return false if num < 2
    return flag
 end
